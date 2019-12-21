87b9a0d1906378a240d1b38d4bba01503d45d388
Levan <gdaylight@gmail.com>
2019-12-21 21:33:06 +0400

    Change Entity "Natural Person"

--- modules/global/src/com/company/davaleba/entity/NaturalPerson.java
+++ modules/global/src/com/company/davaleba/entity/NaturalPerson.java
@@ -2,6 +2,7 @@ package com.company.davaleba.entity;
 
 import com.haulmont.cuba.core.entity.StandardEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -9,4 +10,26 @@ import javax.persistence.Table;
 @Entity(name = "davaleba_NaturalPerson")
 public class NaturalPerson extends StandardEntity {
     private static final long serialVersionUID = -3271034060908410516L;
+
+    @Column(name = "FIRST_NAME_AND_LAST_NAME")
+    protected String firstNameAndLastName;
+
+    @Column(name = "TELEPHONE")
+    protected String telephone;
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFirstNameAndLastName() {
+        return firstNameAndLastName;
+    }
+
+    public void setFirstNameAndLastName(String firstNameAndLastName) {
+        this.firstNameAndLastName = firstNameAndLastName;
+    }
 }
